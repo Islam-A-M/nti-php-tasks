dd8c40f7f4825cde73164e3e2b026d9db5dff836
Islam Ahmed <ra.work99@gmail.com>
2021-11-10 17:49:56 +0200

    nti php task2

---
+++ task2/table.php
@@ -0,0 +1,129 @@
+<?php
+// dynamic table
+// dynamic rows
+// dynamic columns
+// check if gender of user == m ==> male
+// check if gender of user == f ==> female
+
+
+// collection => laravel => array of objects
+
+$users = [
+    (object)[
+        'id' => 1,
+        'name' => 'ahmed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'football', 'swimming', 'running'
+        ],
+        'activities' => [
+            "school" => 'drawing',
+            'home' => 'painting'
+        ],
+    ],
+    (object)[
+        'id' => 2,
+        'name' => 'mohamed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'swimming', 'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ]
+    ],
+    (object)[
+        'id' => 3,
+        'name' => 'mena',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ]
+    ],
+    
+];
+$isHeadersColleted = false;
+$body='<tbody>';
+$table = '<table class="table">';
+$table .= '<thead>
+<tr>';
+$rowCount=0;
+foreach ($users as $key => $value) {
+
+    $rowCount++;
+    $body.='<tr>';
+    //$body.=' <th scope="row">'.  $rowCount.'</th>';
+  foreach ($value as $key => $objectvalue) {
+    
+    if (!$isHeadersColleted) {
+        $table .= '<th scope="col">'.$key.'</th>';
+    }
+    if (gettype($objectvalue)=='array') {
+        $body .= ' <td scope="row">'. implode(',',$objectvalue).'</td>';
+
+    }elseif(gettype($objectvalue)=='object'){
+        $objectBody =[];
+        
+        foreach ($objectvalue as $key => $objectValue) {
+            if ($key=='gender') {
+                $objectValue= $objectValue=='m'?'male':'female';
+            }
+            array_push($objectBody,$objectValue);
+        }
+        $body .= ' <td scope="row">'. implode(',',$objectBody).'</td>';
+
+    }else{
+        $body .= ' <td scope="row">'. $objectvalue.'</td>';
+    
+    }
+}
+$body .='</tr>';
+
+$isHeadersColleted=true;
+}
+
+$table .= '</tr>
+</thead>';
+$table .= $body.' </tbody>';
+
+$table .= '</table>';
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+    <?php
+    echo $table;
+    ?>
+  </body>
+</html>
